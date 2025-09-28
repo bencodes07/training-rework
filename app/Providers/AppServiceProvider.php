ed486bb5a07cf37501072c020dab080622d4ab12
Ben Böckmann <boeckmannben@gmail.com>
2025-09-28 20:41:37 +0100

    force https added

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,6 +25,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (app()->isProduction()) {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
+
         Gate::define('mentor', [EndorsementPolicy::class, 'mentor']);
         Gate::define('endorsements.manage', [EndorsementPolicy::class, 'viewManagement']);
         Gate::define('endorsements.remove-tier1', [EndorsementPolicy::class, 'removeTier1']);
